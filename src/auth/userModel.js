b395c15f7fc16694a000e6ae931da4721b49298a
adrianhuebner <adrianh92@gmail.com>
2021-02-11 21:46:13 -0800

    added validation that email being entered by user is unique

--- src/auth/userModel.js
+++ src/auth/userModel.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const bycrypt = require('bycrpt');
 const jwt = require('jsonwebtoken');
 
-const usersSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
@@ -12,5 +12,20 @@ const usersSchema = new mongoose.Schema({
   username: {
     type: String,
     required: true
+  },
+  password: {
+    type: String,
+    required: true
   }
 })
+
+/**
+ * Validating that email entered is going to be unique so no one can sign up twice
+ */
+userSchema.path('email').validate(async(email) => {
+  const checkingEmail = await mongoose.models.users.countDocuments({ email })
+  return !checkingEmail
+}, 'This email has already been used')
+
+const User = mongoose.model('users', userSchema)
+module.exports = User
