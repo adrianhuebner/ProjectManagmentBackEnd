19e5ed87f1eda4417467b9e16b9daca12cbfd8d2
adrianhuebner <adrianh92@gmail.com>
2021-02-11 21:51:03 -0800

    added .pre to userSchema that will hash the password that user is entering

--- src/auth/userModel.js
+++ src/auth/userModel.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const mongoose = require('mongoose');
-const bycrypt = require('bycrpt');
+const bcrypt = require('bycrpt');
 const jwt = require('jsonwebtoken');
 
 const userSchema = new mongoose.Schema({
@@ -26,6 +26,16 @@ userSchema.path('email').validate(async(email) => {
   const checkingEmail = await mongoose.models.users.countDocuments({ email })
   return !checkingEmail
 }, 'This email has already been used')
+
+/**
+ * Hashing the password that the user is entering
+ */
+userSchema.pre('save', async function(){
+  console.log('About to save the user in the database');
+  if(this.isModified('password')){
+    this.password = await bcrypt.hash(this.password, 10)
+  }
+})
 
 const User = mongoose.model('users', userSchema)
 module.exports = User
