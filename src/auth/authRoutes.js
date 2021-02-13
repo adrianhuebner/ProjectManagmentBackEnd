9c181028439764bb493e35481f8b94f3af7a4950
adrianhuebner <adrianh92@gmail.com>
2021-02-13 12:56:38 -0800

    got auth controller for signup working

--- src/auth/authRoutes.js
+++ src/auth/authRoutes.js
@@ -2,25 +2,8 @@
 
 const express = require('express');
 const authRouter = express.Router();
-const User = require('./userModel.js')
-const { userValidation } = require('../helperFiles/validateUserSchema.js')
+const userController = require('../controllers/userController.js');
 
-authRouter.post('/signup', async (request, response, next) => {
-  try{
-    const result = await userValidation.validateAsync(request.body)
-    let user = new User(result);
-    user.save()
-      .then(user => {
-        request.token = user.generateToken();
-        request.user = user;
-        response.set('token', request.token);
-        response.cookie('auth', request.token);
-        response.send(request.token);
-      })
-  } catch (error){
-    if(error.isJoi === true) error.status = 422
-    next(error)
-  }
-});
+authRouter.post('/signup', userController.signup);
 
 module.exports = authRouter;
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,24 @@
 'use strict';
 
-const { userSchema } = require('../auth/userModel.js');
+const User = require('../auth/userModel.js');
 const { userValidation } = require('../helperFiles/validateUserSchema.js');
+
+module.exports = {
+  signup: async (request, response, next) => {
+    try{
+      const result = await userValidation.validateAsync(request.body)
+      let user = new User(result);
+      user.save()
+        .then(user => {
+          request.token = user.generateToken();
+          request.user = user;
+          response.set('token', request.token);
+          response.cookie('auth', request.token);
+          response.send(request.token)
+        })
+    } catch(error){
+      if(error.isJoi === true) error.status = 422
+      next(error);
+    }
+  }
+}
