462ce668fc9296125bdf58a3672e982d12aeabba
adrianhuebner <adrianh92@gmail.com>
2021-03-08 21:42:31 -0800

    started creating schema for projects

--- src/projects/projectModel.js
+++ src/projects/projectModel.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const mongoose = require('mongoose');
+
+const projectModel = new mongoose.Schema({
+  category: {
+    type: String,
+    required: true,
+    default: 'project'
+  },
+  dateStarted: {
+    type: Date,
+    default: Date.now
+  }
+})
