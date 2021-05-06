0409e0bb65b1d93c1a59346db1036c11e05df417
adrianhuebner <adrianh92@gmail.com>
2021-05-06 00:34:17 -0700

    converted to async/await and working on login test

--- __tests__/userModel.test.js
+++ __tests__/userModel.test.js
@@ -25,39 +25,39 @@ describe('Testing all my user routes', () => {
                       }
   
   describe('Authentication routes', () => {
-    it('An user should be able to create an account', (done) => {
-      return request.post('/signup')
-        .send(testingUser)
-        .then(response => {
-          expect(response.status).toBe(200);
-          done();
-        });
+    it('An user should be able to create an account', async (done) => {
+      const response = await request.post('/signup')
+        .send(testingUser);
+      expect(response.status).toBe(200);
+      done();
     });
 
-    it('An user should be able to sign into their account', (done) => {
-      return request.post('/login')
-        .send('TestingUser', 'TestingStuff102') 
-        .then(response => {
-          expect(response.status).toBe(200);
-          done();
-        });
-    });
-
-    it('User should receive an error if they don\'t enter a proper email', (done) => {
-      return request.post('/signup')
-        .send(badEmailUser)
-        .then(response => {
-          expect(response.status).toBe(500);
-          done();
-        });
-    });
-
-    it('User will recieve a 404 when typing link in wrong', (done) => {
-      return request.post('/badroute')
-        .then(response => {
-          expect(response.status).toBe(404);
-          done();
+    it('An user should be able to sign into their account', async () => {
+      await request.post('/login')
+        .send({
+          username: 'TestingUser',
+          password: 'TestingStuff102'
         })
+    });
+
+    it('An user gets an error when they enter the wrong password', async (done) => {
+      const response = await request.post('/login')
+        .auth('TestingUser', 'WhatIsMyPassword?');
+      expect(response.status).toBe(500);
+      done();
+    });
+
+    it('User should receive an error if they don\'t enter a proper email', async (done) => {
+      const response = await request.post('/signup')
+        .send(badEmailUser);
+      expect(response.status).toBe(500);
+      done();
+    });
+
+    it('User will recieve a 404 when typing link in wrong', async (done) => {
+      const response = await request.post('/badroute');
+      expect(response.status).toBe(404);
+      done();
     });
   });
 });
